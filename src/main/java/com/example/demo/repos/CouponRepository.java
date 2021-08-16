cecaa853a60e3c1d2c8ab3d9c6177341684acdae
talshbt <53658521+talshbt@users.noreply.github.com>
2021-08-16 09:47:55 +0300

    update coupon repo

--- src/main/java/com/example/demo/repos/CouponRepository.java
+++ src/main/java/com/example/demo/repos/CouponRepository.java
@@ -1,11 +1,21 @@
 package com.example.demo.repos;
 
+import com.example.demo.beans.Category;
 import com.example.demo.beans.Coupon1;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Date;
+import java.util.List;
 
 public interface CouponRepository extends JpaRepository<Coupon1, Integer> {
+    List<Coupon1> findByCompanyId(int companyId);
+    List<Coupon1> findByCompanyIdAndCategory(int companyId, Category category);
+    List<Coupon1> findByCompanyIdAndPriceLessThan(int companyId, double maxPrice);
+    List<Coupon1> findByEndDateBefore(Date expiryDate);
 
 
-//    boolean countFindByI(String name);
-//    boolean existsFindByEmail(String email);
+    @Transactional
+    void deleteByEndDateBefore(Date expiryDate);
+
 }
