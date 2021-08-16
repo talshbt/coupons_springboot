b0c3ccf0047bdb86c1a5593d37e800e606960465
talshbt <53658521+talshbt@users.noreply.github.com>
2021-08-16 09:40:46 +0300

    job success

--- src/main/java/com/example/demo/job/CouponRemoval.java
+++ src/main/java/com/example/demo/job/CouponRemoval.java
@@ -3,14 +3,19 @@ package com.example.demo.job;
 import com.example.demo.repos.CouponRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
-
+@Component
 public class CouponRemoval {
     private final CouponRepository couponRepository;
-//
-//    @Scheduled(fixedRate = 1000*60*60*24)
-//    public void execDailyEvent(){
-//
-//    }
+    private static final int DAY = 1000*60*60*24;
+    private static final int Sec10 = 1000*10;
+
+    @Scheduled(fixedRate = Sec10)
+    public void execDailyEvent(){
+
+        System.out.println("**********job running*********");
+
+    }
 }
--- src/main/java/com/example/demo/repos/CouponRepository.java
+++ src/main/java/com/example/demo/repos/CouponRepository.java
@@ -4,6 +4,8 @@ import com.example.demo.beans.Coupon1;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CouponRepository extends JpaRepository<Coupon1, Integer> {
+
+
 //    boolean countFindByI(String name);
 //    boolean existsFindByEmail(String email);
 }
