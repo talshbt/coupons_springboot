c347219632aa29a97de2d0e36e5c70686096f00f
talshbt <53658521+talshbt@users.noreply.github.com>
2021-08-26 10:57:29 +0300

    update

--- src/main/java/com/example/demo/clr/UseCaseAdminService.java
+++ src/main/java/com/example/demo/clr/UseCaseAdminService.java
@@ -1,7 +1,9 @@
 package com.example.demo.clr;
 
 import com.example.demo.beans.Company;
+import com.example.demo.repos.CompanyRepository;
 import com.example.demo.service.AdminService;
+import com.example.demo.utils.Art;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
@@ -12,7 +14,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class UseCaseAdminService implements CommandLineRunner {
     private final AdminService adminService;
-
+    private final CompanyRepository companyRepository;
     @Override
     public void run(String... args) throws Exception {
         System.out.println("before add company");
@@ -23,11 +25,36 @@ public class UseCaseAdminService implements CommandLineRunner {
                 .password("1234")
                 .build();
 
+        Company comp2 = Company.builder()
+                .name("COMP2")
+                .email("comp2@gmail.com")
+                .password("1234")
+                .build();
 
+        Company comp3 = Company.builder()
+                .name("COMP3")
+                .email("comp3@gmail.com")
+                .password("1234")
+                .build();
+
+        System.out.println(Art.INSERT);
         adminService.addCompany(comp1);
+        adminService.addCompany(comp2);
+        adminService.addCompany(comp3);
+
         System.out.println("after add company");
         System.out.println(adminService.getAllCompanies());
 
+        System.out.println(Art.UPDATE);
+        comp1 =  adminService.getSingleCompany(1);
+        comp1.setName("Coca-Cola");
+        adminService.updateCompany(comp1);
+        System.out.println(adminService.getAllCompanies());
+
+        System.out.println(Art.DELETE);
+        comp2 =  adminService.getSingleCompany(2);
+        adminService.deleteCompany(comp2);
+        System.out.println(adminService.getAllCompanies());
 
     }
 }
